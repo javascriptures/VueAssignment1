9fd5e7674249b40ce3e548efaffc49e89c43c9ad
javascriptures <jtjavascripture@gmail.com>
2020-05-27 13:44:38 -0700

    Problem 4

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ new Vue({
 	el: '#exercise',
   data: {
   	name: 'Jordan Taylor',
-    age: '23'
-  },
+    age: '23',
+    link: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+
+},
   methods: {
     randomNum: function() {
         return Math.random();
